e72d126de6ac70962cc9565fd51c1c74feb0c1da
Kevin Valencia <kevinvalencia9436@gmail.com>
2025-08-11 16:21:19 -0400

    Update layout.tsx

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={GeistSans.className}>
+        
         {children}
+        
         <Toaster />
       </body>
     </html>
